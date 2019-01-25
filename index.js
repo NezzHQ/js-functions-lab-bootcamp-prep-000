11cfbe9e314a7c7fd1525858066c48f5587d9126
Bens Hilaire <benshilaire@gmail.com>
2019-01-25 22:26:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+function happyHolidays();
+
+function (happyHolidays(`Happy holidays, ${name}!`);
+
+function happyHolidayTo(`Happy ${holiday}, ${name}!`);
+
+function
+
+function holidayCountdown(`It's ${days} days until ${holiday}!`)
