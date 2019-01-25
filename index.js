0472753fa5a848fadf036ff61ec6185c764031f9
Bens Hilaire <benshilaire@gmail.com>
2019-01-25 23:21:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,12 +3,8 @@ function happyHolidays() {
   return "Happy holidays!"
 } 
 
-function (happyHolidays(`Happy holidays, ${name}!`) {
-  
+function happyHolidaysTo(Jason) {
+  return `Happy holidays, ${name}!`
 }
 
-function happyHolidayTo(`Happy ${holiday}, ${name}!`);
-
-function
-
-function holidayCountdown(`It's ${days} days until ${holiday}!`)
+function happyHolidayTo
