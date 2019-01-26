7c3729a00cc03294e6199c6859ed877cab696791
Bens Hilaire <benshilaire@gmail.com>
2019-01-26 00:56:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ function happyHolidayTo(holiday, name) {
 }
 
 
-function holidayCountdown(holiday, 20) {
-  return "It\'s ${days} days until ${holiday}!"
+function holidayCountdown(holiday, days) {
+return `It\'s ${days} days until ${holiday}!`
 }
 
 
